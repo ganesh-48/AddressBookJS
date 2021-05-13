5b9e8530f56a559b592442225e162e84aec1f573
ganesh-48 <ganeshsrananavare@gmail.com>
2021-05-13 23:54:59 +0530

    ADDED: Vaild contacts added

--- AddressBook.js
+++ AddressBook.js
@@ -1,7 +1,8 @@
 /*@Description-Address Book Problem.
 * Welcome to address book problem.
 * Create a contact in address book.
-* contact first name, last name, address, city, state, zip, phone number and email */
+* contact first name, last name, address, city, state, zip, phone number and email.
+* Check a ensure valid contacts are added . */
 console.log(" Welcome to Address Book Problem ");
 
 class Contacts {
@@ -24,5 +25,122 @@ class Contacts {
         this.zip = [5];
         this.phoneNumber =[6];
         this.email = [7];
+    }
+
+    /**Check the first name start with capatil name.
+     * And it should be minimum 3 charaters.
+     * If it is not start with capatil letter or it is not less than 3 letters then it is invalid.*/
+    get firstName() {
+        return this.firstName;
+    }
+    set firstName(firstName) {
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if (nameRegex.test(firstName)) {
+            this.firstName = firstName;
+        } else {
+            throw "Invalid first name";
+        }
+    }
+
+    /** Check the first name start with capatil name.
+     * And it should be minimum 3 charaters.
+     * If it is not start with capatil letter or it is not less than 3 letters then it is invalid.*/
+    get lastName() {
+        this.lastName;
+    }
+    set lastName(lastName) {
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if (nameRegex.test(lastName)) {
+            this.lastName = lastName;
+        } else {
+            throw "Invalid last name";
+        }
+    }
+
+    /** Check the address is minimum 4 characters.
+     * if is less than then print it is invalid.*/
+    get address() {
+        this.address;
+    }
+    set address(address) {
+        let nameRegex = RegExp("^[A-Za-z0-9]{4,}");
+        if (nameRegex.test(address)) {
+            this.address = address;
+        } else {
+            throw " Address is Invalid ";
+        }
+    }
+
+    /**check the city name is less 4 letters.
+     * In that any numeric value then it is invalid city name. */
+    get city() {
+        this.city;
+    }
+    set city(city) {
+        let nameRegex = RegExp("^[A-Za-z]{4,}$");
+        if (nameRegex.test(city)) {
+            this.city = city;
+        } else {
+            throw "City name is invalid";
+        }
+    }
+
+    /**check the state name is less 2 letters.
+     * In that any numeric value then it is invalid state name. */
+    get state() {
+        this.state;
+    }
+    set state(state) {
+        let nameRegex = RegExp("^[A-Za-z]{2,}$");
+        if (nameRegex.test(state)) {
+            this.state = state;
+        } else {
+            throw "State name is invalid";
+        }
+    }
+
+    /**zip code is in numbers.
+     * if it is less than 6 numbers then it is invalid.*/
+    get zip() {
+        this.zip;
+    }
+    set zip(zip) {
+        let nameRegex = RegExp("^[1-9]{3}[ ]?[0-9]{3}$");
+        if (nameRegex.test(zip)) {
+            this.zip = zip;
+        } else {
+            throw "zip code is invalid";
+        }
+    }
+
+    /**check phone number is 10 digit number .
+     * if not then print a invalid phone number.*/
+    get phoneNumber() {
+        this.phoneNumber;
+    }
+    set phoneNumber(phoneNumber) {
+        let nameRegex = RegExp("^((\\\\+)?(\\\\d{2}[-]))?(\\\\d{10}){1}?$");
+        if (nameRegex.test(phoneNumber)) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw "phone number is invalid";
+        }
+    }
+
+    /**check a email. */
+    get email() {
+        this.email;
+    }
+    set email(email) {
+        let nameRegex = RegExp("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        if (nameRegex.test(email)) {
+            this.email = email;
+        } else {
+            throw "Email id is invalid";
+        }
+    }
+
+    toString() {
+        return "First Name: " + this.firstName + ", Last Name: " + this.lastName+ "Address: " + this.address+ ", City: " + this.city + ", State: "+ this.state + "Zip: " + this.zip+ ", Phone Number: " + this.phoneNumber + ", Email: " + this.email;
     }
 }
