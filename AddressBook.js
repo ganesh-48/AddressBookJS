8739fa5fabc237907355443ced328a67c269fbde
ganesh-48 <ganeshsrananavare@gmail.com>
2021-05-13 22:45:15 +0530

    ADDED: Create Contacts in address book

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,28 @@
+/*@Description-Address Book Problem.
+* Welcome to address book problem.
+* Create a contact in address book.
+* contact first name, last name, address, city, state, zip, phone number and email */
 console.log(" Welcome to Address Book Problem ");
+
+class Contacts {
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(...params) {
+        this.firstName = [0];
+        this.lastName = [1];
+        this.address = [2];
+        this.city = [3];
+        this.state =[4];
+        this.zip = [5];
+        this.phoneNumber =[6];
+        this.email = [7];
+    }
+}
